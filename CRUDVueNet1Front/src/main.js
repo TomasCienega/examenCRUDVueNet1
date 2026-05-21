d2089877ed132d53680474f0542b4015ae1338fc
Tomas Cienega <tomascienega0206@gmail.com>
2026-05-21 16:04:14 -0600

    Se agrego vuetify y se configuro pnpm

--- CRUDVueNet1Front/src/main.js
+++ CRUDVueNet1Front/src/main.js
@@ -2,8 +2,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 1. Importar los estilos de los iconos y de Vuetify 🎨
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+// 2. Importar los creadores de Vuetify
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// 3. Crear la configuración de Vuetify indicando que use los iconos MDI
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi', // Con esto ya sabe que cuando uses un icono, buscará en la fuente MDI
+  },
+})
+
 const app = createApp(App)
 
-app.use(router)
+app.use(router).use(vuetify)
 
 app.mount('#app')
